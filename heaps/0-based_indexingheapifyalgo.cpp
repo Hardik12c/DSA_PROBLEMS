b3005e99f5f1ad5013285d1dd7c9d0f7403d8ed8
Hardik Garg <93364270+Hardik12c@users.noreply.github.com>
2022-10-20 22:36:45 +0530

    heapify algo updated with 0-indexing

--- heaps/0-based_indexingheapifyalgo.cpp
+++ heaps/0-based_indexingheapifyalgo.cpp
@@ -4,27 +4,26 @@ using namespace std;
 
 void heapify(int arr[],int i,int n){
     int largest=i;
-    int left=2*i;
-    int right=2*i+1;
-    if(left<=n && arr[left]>arr[largest]){
+    int left=2*i+1;
+    int right=2*i+2;
+    if(left<n && arr[left]>arr[largest]){
         largest=left;
     }
-    if(right<=n && arr[right]>arr[largest]){
+    if(right<n && arr[right]>arr[largest]){
         largest=right;
     }
-
     if(largest!=i){
         swap(arr[largest],arr[i]);
         heapify(arr,largest,n);
     }
 }
 int main(){
-    int arr[6]={-1,54,53,55,52,50};
+    int arr[5]={54,53,55,52,50};
     int size=5;
-    for(int i=size/2;i>0;i--){
+    for(int i=size/2-1;i>=0;i--){
         heapify(arr,i,size);
     }
-    for(int i=1;i<=size;i++){
+    for(int i=0;i<size;i++){
         cout<<arr[i]<<" ";
     }
     return 0;
