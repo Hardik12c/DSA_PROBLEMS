bf04b434c5bd08e6b8932048d46a643bd6dc1823
Hardik Garg <93364270+Hardik12c@users.noreply.github.com>
2022-11-23 16:36:39 +0530

    Climbing Stairs

---
+++ Dynamic Programming/Climbing Stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int prev1=1;int prev2=1;
+        int curr=prev1;
+        for(int i=2;i<=n;i++){
+            curr=prev1+prev2;
+            prev1=prev2;
+            prev2=curr;
+        }
+        return curr;
+    }
+};
