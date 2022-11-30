02a07397054e393a5749fe98f2f585699d533702
Hardik Garg <93364270+Hardik12c@users.noreply.github.com>
2022-11-30 22:10:37 +0530

    triangle

---
+++ Dynamic Programming/triangle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int solve(vector<vector<int>> &triangle ,int i, int j,vector<vector<int>> &dp){
+        if(i>=triangle.size()){
+            return 0;
+        }
+        if(dp[i][j]!=-1) return dp[i][j];
+        int down=triangle[i][j]+solve(triangle,i+1,j,dp);
+        int rightdown=triangle[i][j]+solve(triangle,i+1,j+1,dp);
+
+        return dp[i][j]= min(down,rightdown);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return solve(triangle,0,0,dp);
+    }
+};
