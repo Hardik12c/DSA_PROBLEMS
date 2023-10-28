4ffe018485f8842e06d11b0e8db1e08caef48a3e
Hardik Garg <hardik12c@gmail.com>
2023-10-28 11:55:55 +0530

    unique occurence

--- array/Unique Number of Occurrences.cpp
+++ array/Unique Number of Occurrences.cpp
@@ -15,3 +15,22 @@ public:
         return true;
     }
 };
+
+// another solution
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int> mpp;
+        for(int i=0;i<arr.size();i++){
+            mpp[arr[i]]++;
+        }
+        unordered_map<int,int> occMpp;
+        for(auto e:mpp){
+            if(occMpp.find(e.second)!=occMpp.end()){
+                return false;
+            }
+            occMpp[e.second]=e.first;
+        }
+        return true;
+    }
+};
