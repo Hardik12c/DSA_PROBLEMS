f027a734457ed8fa57c337ae974ac1f51464b5d5
Hardik Garg <hardik12c@gmail.com>
2023-11-12 01:57:21 +0530

    Concatenation of Array

---
+++ array/Concatenation of Array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans;
+        int n=nums.size();
+        for(int i=0;i<n*2;i++){
+            if(i>=n){
+                ans.push_back(nums[i-n]);
+            }else
+                ans.push_back(nums[i]);
+        }
+        return ans;
+    }
+};
