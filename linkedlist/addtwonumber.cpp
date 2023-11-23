7c967333adb14afa6b3bc99b2722be3969589f71
Hardik Garg <hardik12c@gmail.com>
2023-11-24 01:29:14 +0530

    add two numbers in a linked list

--- linkedlist/addtwonumber.cpp
+++ linkedlist/addtwonumber.cpp
@@ -76,3 +76,54 @@ public:
         return ans;
     }
 };
+
+
+//another solution with less time complexity
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* head=new ListNode(-1);
+        ListNode* temp=head;
+        int carry=0;
+        while(l1!=NULL && l2!=NULL){
+            cout<<" hi"<<endl;
+            int sum=l1->val+l2->val+carry;
+            temp->next=new ListNode((sum)%10);
+            carry=sum/10;
+            temp=temp->next;
+            l1=l1->next;
+            l2=l2->next;
+
+        }
+        while(l1!=NULL){
+            int sum=l1->val+carry;
+            temp->next=new ListNode((sum)%10);
+            carry=sum/10;
+            temp=temp->next;
+            l1=l1->next;
+        }
+        while(l2!=NULL){
+            int sum=l2->val+carry;
+            temp->next=new ListNode((sum)%10);
+            carry=sum/10;
+            temp=temp->next;
+            l2=l2->next;
+        }
+        while(carry!=0){
+            temp->next=new ListNode((carry)%10);
+            carry/=10;
+            temp=temp->next;
+        }
+        return head->next;
+    }
+};
