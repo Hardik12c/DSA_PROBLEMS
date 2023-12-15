86c7464cc639e22eabb0f03f9fe51b3edff35424
Hardik Garg <hardik12c@gmail.com>
2023-12-15 10:29:54 +0530

    Remove All Occurrences of a Substring

--- string/Remove All Occurrences of a Substring.cpp
+++ string/Remove All Occurrences of a Substring.cpp
@@ -7,3 +7,21 @@ public:
         return s;
     }
 };
+
+//another solution
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        string another=s;
+        int j=0;
+        for(int i=0;i<s.length();i++){
+            another[j]=s[i];
+            j++;
+            if(j>=part.size() && another.substr(j-part.size(),part.size())==part){
+                j-=part.size();
+            }
+        }
+
+        return another.substr(0,j);
+    }
+};
