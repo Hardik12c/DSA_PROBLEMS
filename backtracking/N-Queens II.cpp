8638923cf69dc63670b1ecab2f773abb6e7a4667
Hardik Garg <hardik12c@gmail.com>
2024-01-01 23:03:38 +0530

    N-Queens II

--- backtracking/N-Queens II.cpp
+++ backtracking/N-Queens II.cpp
@@ -34,3 +34,32 @@ public:
         return ans.size();
     }
 };
+
+//optimized and good solution
+class Solution {
+public:
+    int recurse(int ind ,int n,vector<int> row,vector<int> upper,vector<int> down){
+        if(ind>=n){
+            return 1;
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(!row[i] && !upper[n+ind-i-1] && !down[ind+i]){
+                row[i]=1;
+                upper[n+ind-i-1]=1;
+                down[ind+i]=1;
+                count+=recurse(ind+1,n,row,upper,down);
+                row[i]=0;
+                upper[n+ind-i-1]=0;
+                down[ind+i]=0;
+            }
+        }
+        return count;
+    }
+    int totalNQueens(int n) {
+        vector<int> row(n,0);
+        vector<int> upper(2*n,0);
+        vector<int> down(2*n,0);
+        return recurse(0,n,row,upper,down);
+    }
+};
