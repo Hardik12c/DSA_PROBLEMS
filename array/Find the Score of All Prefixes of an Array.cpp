48db59864687e1213e1633cd5270c7fbae7840c9
Hardik Garg <hardik12c@gmail.com>
2024-01-19 08:39:20 +0530

    Find the Score of All Prefixes of an Array

---
+++ array/Find the Score of All Prefixes of an Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<long long> findPrefixScore(vector<int>& nums) {
+        long long mx=INT_MIN;
+        vector<long long> ans;
+        long long sum=0;
+        for(int i=0;i<nums.size();i++){
+            mx=mx<nums[i]?nums[i]:mx;
+            int conver=nums[i]+mx;
+            sum+=conver;
+            ans.push_back(sum);
+        }
+        return ans;
+    }
+};
